b96f91f8258acb929c963adda531ef293bcbc02a
Tin9oo <47194825+Tin9oo@users.noreply.github.com>
2023-11-22 17:15:06 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.04 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 숫자 문자열과 영단어 - 81301 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301?language=cpp) 
 
 ### 성능 요약
 
-메모리: 3.62 MB, 시간: 0.01 ms
+메모리: 4.14 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 3일 16:41:23
+2023년 11월 3일 17:15:3
 
 ### 문제 설명
 
--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
@@ -1,39 +1,57 @@
 #include <string>
 #include <vector>
 
+#include <regex>
+
 using namespace std;
 
-vector<string> words = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+// vector<string> words = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
 int solution(string s) {
-    long long answer = 0;
+//     long long answer = 0;
     
-    bool is_matched=true;
+//     bool is_matched=true;
     
-    for(int idx_s = 0; idx_s < s.length(); ++idx_s) {
-        if('0'<=s[idx_s]&&s[idx_s]<='9') {
-            answer+=s[idx_s]-'0';
-            answer*=10;
-            continue;
-        }
-        for(int i = 0; i < words.size(); ++i) {
-            is_matched=true;
-            for(int j = 0; j < words[i].length(); ++j) {
-                if(s[idx_s+j] != words[i][j]) {
-                    is_matched=false;
-                    break;
-                }
-            }
-            if(is_matched==true) {
-                answer+=i;
-                answer*=10;
+//     for(int idx_s = 0; idx_s < s.length(); ++idx_s) {
+//         if('0'<=s[idx_s]&&s[idx_s]<='9') {
+//             answer+=s[idx_s]-'0';
+//             answer*=10;
+//             continue;
+//         }
+//         for(int i = 0; i < words.size(); ++i) {
+//             is_matched=true;
+//             for(int j = 0; j < words[i].length(); ++j) {
+//                 if(s[idx_s+j] != words[i][j]) {
+//                     is_matched=false;
+//                     break;
+//                 }
+//             }
+//             if(is_matched==true) {
+//                 answer+=i;
+//                 answer*=10;
                 
-                idx_s+=words[i].length()-1;
-            }
-        }
-    }
+//                 idx_s+=words[i].length()-1;
+//             }
+//         }
+//     }
     
-    answer/=10;
+//     answer/=10;
+    
+    // 풀이 2
+    int answer=0;
+    
+    s = regex_replace(s, regex("zero"), "0");
+    s = regex_replace(s, regex("one"), "1");
+    s = regex_replace(s, regex("two"), "2");
+    s = regex_replace(s, regex("three"), "3");
+    s = regex_replace(s, regex("four"), "4");
+    s = regex_replace(s, regex("five"), "5");
+    s = regex_replace(s, regex("six"), "6");
+    s = regex_replace(s, regex("seven"), "7");
+    s = regex_replace(s, regex("eight"), "8");
+    s = regex_replace(s, regex("nine"), "9");
+    
+    answer = stoi(s);
     
     return answer;
 }
