81f7f2149dba2fea48df10665f5306b985b7822c
Tin9oo <47194825+Tin9oo@users.noreply.github.com>
2024-07-27 17:37:53 +0900

    [level 2] Title: 숫자의 표현, Time: 0.04 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -3,17 +3,32 @@ import java.util.*;
 
 class Solution {
     public int solution(int n) {
-        int answer = 0;
+        // 풀이 1
+//         int answer = 0;
         
-        for(int i = 1; i <= n; i++) {
-            int tmp = 0;
-            for(int j = 0; j < n; j++) {
-                if(tmp+i+j > n) break;
-                else if(tmp+i+j == n) answer++;
-                tmp += i+j;
+//         for(int i = 1; i <= n; i++) {
+//             int tmp = 0;
+//             for(int j = 0; j < n; j++) {
+//                 if(tmp+i+j > n) break;
+//                 else if(tmp+i+j == n) answer++;
+//                 tmp += i+j;
+//             }
+//         }
+        
+//         return answer;
+        
+        // 풀이 2
+
+        int count = 0;
+        
+        // k는 연속된 자연수의 개수
+        for (int k = 1; k * (k + 1) / 2 <= n; k++) {
+            // n이 k개의 연속된 자연수의 합으로 표현될 수 있는지 확인
+            if ((n - k * (k + 1) / 2) % k == 0) {
+                count++;
             }
         }
         
-        return answer;
+        return count;
     }
 }
